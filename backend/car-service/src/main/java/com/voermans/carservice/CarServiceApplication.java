0bc5164c67f4864d4239940f4935e24a8daa0546
RickyStruuz <ricky54321@live.nl>
2020-06-21 19:47:54 +0200

    Things forgot to push

--- backend/car-service/src/main/java/com/voermans/carservice/CarServiceApplication.java
+++ backend/car-service/src/main/java/com/voermans/carservice/CarServiceApplication.java
@@ -8,22 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CarServiceApplication implements CommandLineRunner {
-
-    @Autowired
-    private CarService carService;
+public class CarServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CarServiceApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        // Initialize db with dummy data to test:
-        carService.saveCar(new Car("12-XZG-1", "Opel", "Astra", "Karel de Groot"));
-        carService.saveCar(new Car("20-VVG-0", "Honda", "2", "Karel de Groot"));
-        carService.saveCar(new Car("1-SFL-99", "Volkswagen", "Polo", "Piet Klerx"));
-        carService.saveCar(new Car("99-AZ-P2", "Ford", "Fiesta", "Kees de Koning"));
-        carService.saveCar(new Car("32-VG-PP", "Toyota", "Yaris", "Rick Voermans"));
     }
 }
